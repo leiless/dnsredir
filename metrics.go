7909f4e07f899ca07006ba650a7fe1283fcf7930
leixiang <lynnl.yet@gmail.com>
2020-03-23 14:15:50 +0800

    metrics.go: Use fine-grained time buckets for NameLookupDuration

--- metrics.go
+++ metrics.go
@@ -6,11 +6,16 @@ import (
 )
 
 var (
+	// Time buckets used for name lookup duration in milliseconds
+	nameLookupBuckets = []float64{
+		1, 5, 10, 15, 20, 25, 30, 35, 40, 50, 60, 80, 100, 125, 150,
+	}
+
 	NameLookupDuration = prometheus.NewHistogramVec(prometheus.HistogramOpts{
 		Namespace: plugin.Namespace,
 		Subsystem: pluginName,
 		Name: "name_lookup_duration_ms",
-		Buckets:   plugin.TimeBuckets,
+		Buckets: nameLookupBuckets,
 		Help: "Histogram of the time(in milliseconds) each name lookup took.",
 	}, []string{"status"})
 )

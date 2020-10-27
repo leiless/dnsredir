bdbba97a1dfb752959dd3c6548c080a9ed44837c
leixiang <lynnl.yet@gmail.com>
2020-10-27 09:38:00 +0800

    pf: Add download Darwin pf headers shell script

---
+++ pf/download-pf-headers.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -euf
+#set -x
+
+if [ "$(uname -s)" != "Darwin" ]; then
+    echo "This script only available on macOS"
+    exit 1
+fi
+
+cd "$(dirname "$0")"
+DIR=darwin-xnu
+
+echo Downloading pf headers...
+
+mkdir -p "$DIR/net"
+curl -sL -o "$DIR/net/pfvar.h" https://raw.githubusercontent.com/apple/darwin-xnu/master/bsd/net/pfvar.h
+curl -sL -o "$DIR/net/radix.h" https://raw.githubusercontent.com/apple/darwin-xnu/master/bsd/net/radix.h
+
+mkdir -p "$DIR/libkern"
+curl -sL -o "$DIR/libkern/tree.h" https://raw.githubusercontent.com/apple/darwin-xnu/master/libkern/libkern/tree.h
+
+echo Done
+
